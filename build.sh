ecad3f7438cd584ce1f395fd0f44b0247b95546b
Mohammad younis khan <135511341+Youniskhan8@users.noreply.github.com>
2025-06-09 21:49:43 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ pip install gdown
 
 # Download the model
 echo "Downloading model files..."
-gdown --id 1lVg8_ue8e9B-0m8oeuTMeXt0MWoVJERa" -O model.pkl
+gdown --id 1lVg8_ue8e9B-0m8oeuTMeXt0MWoVJERa -O model.pkl
+
 
